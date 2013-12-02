67ed0340c4f0e425a39d4fa9c813cab72efac394
KhalenMcDonnell <Khalenm@gmail.com>
2013-12-01 23:48:38 -0800

    Update route.js

--- route.js
+++ route.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var mysql = require('mysql');
 var app = express();
@@ -19,46 +20,73 @@ app.use(express.static(__dirname + '/'));
 
 //login Authentication.
 
+exports.login = function(user, callback){
+        accounts.findOne({username: user}, function(err, result){
+                if (err) throw err;
+                callback(result);
+        });
+};
+
+var readAccount = function(user, callback){
+        AM.login(user, function(acct){
+                callback(acct);
+        });
+};
+
+exports.loginHandler = function(req, res) {
+        username = req.body.username;
+        password = req.body.password;
+        readAccount(username, function(acct){
+                if (acct.password = password){
+                        req.session.username = username;
+                        console.log(username+' logged in'+' pass: '+ acct.password);
+                };
+                res.redirect('/')
+        });
+};
+
+
+
 
 app.post('/loginAuth', function(req, res, err) { 
-	var loginUser = req.body.username;
-	var loginPass = req.body.password;
-	connection.query('SELECT username,password FROM user WHERE username = ? && password = ?', [loginUser, loginPass], function(err, rows, fields) {
-		console.log("this is an error " + err);
-	});
+        var loginUser = req.body.username;
+        var loginPass = req.body.password;
+        connection.query('SELECT username,password FROM user WHERE username = ? && password = ?', [loginUser, loginPass], function(err, rows, fields) {
+                console.log("this is an error " + err);
+        });
 
-	if (!err) {
-		console.log("Success!");
-		res.redirect('/easyform.html');
-	} else {
-		console.log('post error: ' + err);
-		res.redirect('/loginfailed.html');
-	}
+        if (!err) {
+                console.log("Success!");
+                res.redirect('/easyform.html');
+        } else {
+                console.log('post error: ' + err);
+                res.redirect('/loginfailed.html');
+        }
 });
 
 /*res.statusCode = 302;
-		res.setHeader("Order Form", "/easyform.html");
-		res.end(); */
+                res.setHeader("Order Form", "/easyform.html");
+                res.end(); */
 
 
 //Route for IMEI Order
 app.post('/keys', function(req, res) {
-	var IMEI = req.body.imei,
-		phoneType = req.body.model,
-		activeUser = req.body.username
-	connection.query('INSERT INTO imei_order (imei, model, username) VALUES (?, ?, ?)', [IMEI, phoneType, activeUser], function(err, rows, fields) {
-		console.log(err, rows, fields);
-	});
+        var IMEI = req.body.imei,
+                phoneType = req.body.model,
+                activeUser = req.body.username;
+        connection.query('INSERT INTO imei_order (imei, model, username) VALUES (?, ?, ?)', [IMEI, phoneType, activeUser], function(err, rows, fields) {
+                console.log(err, rows, fields);
+        });
 
-	console.info('login PARAM ', req.body);
+        console.info('login PARAM ', req.body);
 
-	res.redirect('/success.html')
+        res.redirect('/success.html')
 });
 
 app.get('/imei', function(req, res){
-	connection.query('SELECT * FROM imei_vault', function(err, rows) {
-		res.render('display', {imei_vault: rows});
-	});
+        connection.query('SELECT * FROM imei_vault', function(err, rows) {
+                res.render('display', {imei_vault: rows});
+        });
 });
 app.listen(5454);
 console.log("LISTENING ON 5454");
